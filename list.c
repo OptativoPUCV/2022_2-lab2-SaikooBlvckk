ec2f1c624e0603cb0c3b238b88fa7835426450d8
Replit user <>
2022-08-20 20:13:47 +0000

    new try

--- list.c
+++ list.c
@@ -113,8 +113,8 @@ void * popCurrent(List * list) {
     current->next->prev = NULL;
     list->head = current->next;
   }
-  current->prev->next = current->next;
-  current->next->prev = current->prev;
+  current->prev->next = list->current->next;
+  current->next->prev = list->current->prev;
 
   free(current);
   return dato;
